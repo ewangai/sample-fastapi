1f3ff2bd44af97552d23dd9e06280a4aedc6b3b7
ewangai <ewangai@hotmail.com>
2023-10-30 18:34:14 +0000

    added exclamation

--- app/main.py
+++ app/main.py
@@ -42,7 +42,7 @@ app.include_router(vote.router)
 #Path operation or Route
 @app.get("/")                           # Decorator - This turns htis into an path operator to the endpoint is usable e.b. a get method plus path
 async def root():                       # Function - async is optional but required when asynchronous task (takes time) name of the function here is root but it can be anything. root is default
-    return {"message": "Welcome to my API"}
+    return {"message": "Welcome to my API!!!"}
 
 
 
