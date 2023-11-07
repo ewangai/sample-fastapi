c54f694f9ddbe8ef67f553e0d07bcb24e2d55e5d
ewangai <ewangai@hotmail.com>
2023-11-07 18:50:52 +0000

    HEroku eg

--- app/main.py
+++ app/main.py
@@ -39,7 +39,7 @@ app.include_router(vote.router)
 #Path operation or Route
 @app.get("/" )                         # Decorator - This turns htis into an path operator to the endpoint is usable e.b. a get method plus path
 async def root():                       # Function - async is optional but required when asynchronous task (takes time) name of the function here is root but it can be anything. root is default
-    return {"message": "Hello World"}
+    return {"message": "Hello World CICD"}
 
 
 
