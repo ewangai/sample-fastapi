605d6abafef209c06d6157b4e6df44dc5740dee3
ewangai <ewangai@hotmail.com>
2023-12-11 12:32:42 +0000

    fixed DB port issue

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ import time
 from .config import settings
 
 #SQLALCHEMY_DATABASE_URL = "postgresql://<username>:<password>@<ip address>/<hostname>/<databse_name>"
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 #Issue exposing username and password.
 
 
